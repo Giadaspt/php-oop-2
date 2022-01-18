5968ddabeb57ecaed99e7f274a65c2c285775c05
Giadaspt <saputo.giada@gmail.com>
2022-01-18 15:34:14 +0100

    making shop

---
+++ classes/Shop.php
@@ -0,0 +1,35 @@
+<?php
+
+class Shop {
+  private $name;
+  private $store;
+  private $cart;
+  private $discount;
+
+  function __construct($_name)
+  {
+    $this->name = $_name;
+  }
+
+  public function setStore($_store){
+    $this->store = $_store;
+  }
+  public function setCart($_cart){
+    $this->cart = $_cart;
+  }
+  public function setDiscount($_discount){
+    $this->discount = $_discount;
+  }
+
+  public function getStore(){
+    return $this->store;
+  }
+  public function getCart(){
+    return $this->cart;
+  }
+  public function getDiscount(){
+    return $this->discount;
+  }
+
+
+}
--- index.php
+++ index.php
@@ -1,15 +1,27 @@
-/*
-Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+
+<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
 Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
 Provate a far interagire tra di loro gli oggetti: ad esempio, l’utente dello shop inserisce una carta di credito...
 $c = new CreditCard(..);
 $user->insertCreditCard($c);
 BONUS:
-Gestite eventuali eccezioni che si possono verificare (es: carta di credito scaduta).
+Gestite eventuali eccezioni che si possono verificare (es: carta di credito scaduta). -->
 
-*/
+
 
 <?php
+
+  require_once __DIR__ . "/classes/Shop.php";
+  require_once __DIR__ . "/classes/ShopProduct.php";
+  require_once __DIR__ . "/classes/User.php";
+  require_once __DIR__ . "/classes/UserPremium.php";
+  require_once __DIR__ . "/classes/Payment.php";
+  require_once __DIR__ . "/classes/PayCard.php";
+  require_once __DIR__ . "/classes/PayCashOnDelivery.php";
+
+  $new_shop = new Shop("Ciccia Buffa S.r.l.");
+  var_dump($new_shop);
+
 ?>
 
 <!DOCTYPE html>
